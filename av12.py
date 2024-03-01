e40c171b80fa777e4b576018160c66f9a486cdac
OkiljonMuhammad <142194331+OkiljonMuhammad@users.noreply.github.com>
2024-03-01 10:47:12 +0500

    Update av12.py

--- av12.py
+++ av12.py
@@ -1,19 +1,19 @@
 from typing import List
 #1-masala
 
-# def sumOddLengthSubarrays(arr: List[int]) -> int:
-#         total_sum = 0
-#         n = len(arr)
-#         for i in range(n):
-#             for j in range(i, n, 2):
-#                 total_sum += sum(arr[i:j+1])
-#         return total_sum
+def sumOddLengthSubarrays(arr: List[int]) -> int:
+        total_sum = 0
+        n = len(arr)
+        for i in range(n):
+            for j in range(i, n, 2):
+                total_sum += sum(arr[i:j+1])
+        return total_sum
     
-# arr = [1,4,2,5,3]
+arr = [1,4,2,5,3]
 
-# result = sumOddLengthSubarrays(arr)
+result = sumOddLengthSubarrays(arr)
 
-# print(result)
+print(result)
 
 #2-masala
 
